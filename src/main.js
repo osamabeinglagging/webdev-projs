1c26d417e8ebe417b3ca40d27db18116b6f9cbfb
osamabeinglagging <zubayersamurai@gmail.com>
2024-03-21 10:18:32 +0600

    Push before os change

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../components/header.css';
+import '../components/hero.css';
+import '../components/about.css';
+import '../components/featured.css';
+import '../styles/utils.css';
+
